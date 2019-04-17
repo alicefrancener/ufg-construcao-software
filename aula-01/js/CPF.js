44121b1665c336e136e615e3d22b1bdad2cbbcb5
Alice Francener <alicefrancener@gmail.com>
2019-04-16 23:20:36 -0300

    inclusão da função converteCaracteresEmInteiros

--- aula-01/js/CPF.js
+++ aula-01/js/CPF.js
@@ -1,44 +1,41 @@
+function converteCaracteresEmInteiros(cpf) {
+  var cpfVetor = [];
+  for (var i = 0; i < cpf.length; i++) {
+    cpfVetor[i] = parseInt(cpf.charAt(i));
+  }
+  return cpfVetor;
+}
 
-function validarDigitosCPF(cpf){
-  if(cpf.length != 11){
-    // TODO RangeError captura melhor esta situação em vez de Error.
-    throw new Error("Argumento inválido, CPF deve ter 11 dígitos");
+function validarDigitosCPF(cpf) {
+  if (cpf.length != 11) {
+    throw new RangeError("CPF deve ter 11 dígitos: " + cpf);
   }
-  
-  // TODO parseInt espalhado pelo código, por que não localizar?
-  var j = parseInt(cpf.charAt(0));
-  var k = parseInt(cpf.charAt(1));
-  for(var i = 1; i < 9; i++){
-    j += parseInt(cpf.charAt(i))*(i+1);
+  var cpfVetor = converteCaracteresEmInteiros(cpf);
+  var j = cpfVetor[0];
+  var k = cpfVetor[1];;
+  for (var i = 1; i < 9; i++) {
+    j += cpfVetor[i] * (i + 1);
   }
-  for(var i = 2; i < 10; i++){
-    k += parseInt(cpf.charAt(i))*(i+1);
+  for (var i = 2; i < 10; i++) {
+    k += cpfVetor[i] * i;
   }
   var dj = (j % 11) % 10;
   var dk = (k % 11) % 10;
-  if(dj == parseInt(cpf.charAt(9)) && dk == parseInt(cpf.charAt(10))){
-    return true;
-  }
-  return false;
+  return (dj == cpfVetor[9] && dk == cpfVetor[10]);
 }
 
-function validarDigitosCPF2(cpf){
-  if(cpf.length != 11){
-    throw new Error("Argumento inválido, CPF deve ter 11 dígitos");
+function validarDigitosCPF2(cpf) {
+  if (cpf.length != 11) {
+    throw new RangeError("CPF deve ter 11 dígitos: " + cpf);
   }
-  var p = parseInt(cpf.charAt(8));
+  var cpfVetor = converteCaracteresEmInteiros(cpf);
+  var p = cpfVetor[8];
   var s = p;
-  for(var c = 7; c >=0; c--){
-    p += parseInt(cpf.charAt(c));
+  for (var c = 7; c >= 0; c--) {
+    p += cpfVetor[c];
     s += p;
   }
   var j = (s % 11) % 10;
-  var k = ((s - p + 9*parseInt(cpf.charAt(9))) % 11) % 10;
-  // if(j === parseInt(cpf.charAt(9)) && k === parseInt(cpf.charAt(10))){
-  //  return true;
-  // }
-  // return false;
-  
-  // Lembra-se do "if" do código do João Carlos, era similar ao comentado acima.
-  return j === parseInt(cpf.charAt(9)) && k === parseInt(cpf.charAt(10));
+  var k = ((s - p + 9 * cpfVetor[9]) % 11) % 10;
+  return j === cpfVetor[9] && k === cpfVetor[10];
 }

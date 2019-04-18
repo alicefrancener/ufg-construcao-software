4dd32d0f9d9b5dbca0e01176766633a88d21eb4f
Alice Francener <alicefrancener@gmail.com>
2019-04-18 17:32:09 -0300

    mudança nas exceções

--- aula-01/java/Matematica.java
+++ aula-01/java/Matematica.java
@@ -2,53 +2,43 @@
 public class Matematica {
 
   public int mod(int x, int y) {
-    if (y >= 0 & x > 0) {
-      int resto = x;
-      
-      // Em defesa da legibilidade => manutenibilidade
-      // (remova estes comentários)
-      // for (; y <= resto; resto -= y)
-      // ;
-      
-      // while (y <= resto) {
-      //   resto = resto - y;
-      // }
-      
-      while (resto >= y) {
-        resto = resto - y;
-      }
-      
-      return resto;
+    if (y < 0 || x <= 0) {
+      throw new IllegalArgumentException("x ou y inválido: x = " + x + ", y = " + y);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: y >=0 & x > 0");
+    int resto = x;
+    while (resto >= y) {
+      resto = resto - y;
+    }
+    return resto;
   }
 
   public int produto(int a, int b) {
-    if (a >= 0 & b >= 0) {
-      int totalParcelas = a;
-      int parcela = b;
-      if (b < a) {
-        totalParcelas = b;
-        parcela = a;
-      }
-      int soma = 0;
-      for (int i = 1; i <= totalParcelas; i += 1) {
-        soma += parcela;
-      }
-      return soma;
+    if (a < 0 || b < 0) {
+      throw new IllegalArgumentException("a ou b inválido: a = " + a + ", b = " + b);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: a>=0 & b>=0");
+    int totalParcelas = a;
+    int parcela = b;
+    if (b < a) {
+      totalParcelas = b;
+      parcela = a;
+    }
+    int soma = 0;
+    for (int i = 1; i <= totalParcelas; i += 1) {
+      soma += parcela;
+    }
+    return soma;
   }
 
   public int potencia(int base, int expoente) {
-    if (base >= 0 & expoente >= 0) {
-      int potencia2 = 1;
-      for (int i = 1; i <= expoente; i++) {
-        potencia2 = produto(potencia2, base);
-      }
-      return potencia2;
+    if (base < 0 || expoente < 0) {
+      throw new IllegalArgumentException("base ou expoente inválido: base = " + base + ", expoente = " + expoente);
+
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: base>=0 & expoente>=0");
+    int potencia2 = 1;
+    for (int i = 1; i <= expoente; i++) {
+      potencia2 = produto(potencia2, base);
+    }
+    return potencia2;
   }
 
   public boolean intervalo(int numero, int intervaloInferior, int intervaloSuperior) {
@@ -58,198 +48,198 @@ public class Matematica {
   }
 
   public boolean propriedade3025(int numero) {
-    if (intervalo(numero, 0, 9999)) {
-      int quociente = numero / 100;
-      int resto = mod(numero, 100);
-      int resultado = potencia(quociente + resto, 2);
-      return numero == resultado;
+    if (!intervalo(numero, 0, 9999)) {
+      throw new IllegalArgumentException("numero inválido: " + numero);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: 0 <= numero <= 9999");
+    int quociente = numero / 100;
+    int resto = mod(numero, 100);
+    int resultado = potencia(quociente + resto, 2);
+    return numero == resultado;
   }
 
   public boolean propriedade153(int numero) {
-    if (intervalo(numero, 100, 999)) {
-      int quociente = numero / 100;
-      int resto = mod(numero, 100);
-      int quociente2 = resto / 10;
-      int resto2 = mod(resto, 10);
-      int resultado = potencia(quociente, 3) + potencia(quociente2, 3) + potencia(resto2, 3);
-      return resultado == numero;
+    if (!intervalo(numero, 100, 999)) {
+      throw new IllegalArgumentException("numero inválido: " + numero);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: 100 <= numero <= 999");
+    int quociente = numero / 100;
+    int resto = mod(numero, 100);
+    int quociente2 = resto / 10;
+    int resto2 = mod(resto, 10);
+    int resultado = potencia(quociente, 3) + potencia(quociente2, 3) + potencia(resto2, 3);
+    return resultado == numero;
   }
 
   public int somaNaturais(int numero) {
-    if (numero >= 1) {
-      int soma = 1;
-      for (int i = 2; numero >= i; i++) {
-        soma += i;
-      }
-      return soma;
+    if (numero < 1) {
+      throw new IllegalArgumentException("numero inválido: " + numero);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: numero >= 1");
+    int soma = 1;
+    for (int i = 2; numero >= i; i++) {
+      soma += i;
+    }
+    return soma;
   }
 
   public int fatorial(int numero) {
-    if (numero >= 1) {
-      int fatorial = 1;
-      for (int i = 2; i <= numero; i++) {
-        fatorial *= i;
-      }
-      return fatorial;
+    if (numero < 1) {
+      throw new IllegalArgumentException("numero inválido: " + numero);
+
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: numero >= 1");
+    int fatorial = 1;
+    for (int i = 2; i <= numero; i++) {
+      fatorial *= i;
+    }
+    return fatorial;
   }
 
   public double pi(int n) {
-    if (n >= 1) {
-      double p = 0, s = -1, d = -1;
-      for (int i = 1; i <= n; i++) {
-        d += 2;
-        s = -1 * s;
-        p += 4 * s / d;
-      }
-      return p;
+    if (n < 1) {
+      throw new IllegalArgumentException("n inválido: " + n);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: n >= 1");
+    double p = 0, s = -1, d = -1;
+    for (int i = 1; i <= n; i++) {
+      d += 2;
+      s = -1 * s;
+      p += 4 * s / d;
+    }
+    return p;
   }
 
   public double logaritmoNatural(double n, double k) {
-    if (n >= 1 & k >= 2) {
-      double e = 1 + n;
-      double numerador = n;
-      double denominador = 1;
-      for (double i = 2; i <= k; i++) {
-        numerador *= numerador;
-        denominador += i;
-        e += numerador / denominador;
-      }
-      return e;
+    if (n < 1 || k < 2) {
+      throw new IllegalArgumentException("n ou k inválido: n = " + n + ", k = " + k);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: n >= 1 & k >= 2");
+    double e = 1 + n;
+    double numerador = n;
+    double denominador = 1;
+    for (double i = 2; i <= k; i++) {
+      numerador *= numerador;
+      denominador += i;
+      e += numerador / denominador;
+    }
+    return e;
   }
 
   public double razaoAurea(double x, double y, double k) {
-    if (x >= 0 & x < y & k > 0) {
-      double c = y;
-      double a = x;
-      for (int i = 1; i <= k; i++) {
-        double t = c;
-        c += a;
-        a = t;
-      }
-      return c / a;
+    if (x < 0 || x >= y || k <= 0) {
+      throw new IllegalArgumentException("x, y ou k inválido: x = " + x + ", y = " + y + ", k = " + k);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: x >= 0 & x < y & k > 0");
+    double c = y;
+    double a = x;
+    for (int i = 1; i <= k; i++) {
+      double t = c;
+      c += a;
+      a = t;
+    }
+    return c / a;
   }
 
   public boolean quadradoPerfeito(int numero) {
-    if (numero >= 1) {
-      int s = 1;
-      for (int i = 3; s < numero; i += 2) {
-        s += i;
-      }
-      return s == numero;
+    if (numero < 1) {
+      throw new IllegalArgumentException("numero inválido: " + numero);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: numero >= 1");
+    int s = 1;
+    for (int i = 3; s < numero; i += 2) {
+      s += i;
+    }
+    return s == numero;
   }
 
   public double raiz(double numero, int i) {
-    if (numero > 0) {
-      double r = 1;
-      for (; i >= 0; i--) {
-        r = (r + numero / r) / 2;
-      }
-      return r;
+    if (numero <= 0) {
+      throw new IllegalArgumentException("numero inválido: " + numero);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: numero > 0");
+    double r = 1;
+    for (; i >= 0; i--) {
+      r = (r + numero / r) / 2;
+    }
+    return r;
   }
 
   public boolean primo(int numero) {
-    if (numero > 1) {
-      for (int i = 2; i < numero; i++) {
-        if (mod(numero, i) == 0) {
-          return false;
-        }
-      }
-      return true;
+    if (numero <= 1) {
+      throw new IllegalArgumentException("numero inválido: " + numero);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: numero > 1");
+    for (int i = 2; i < numero; i++) {
+      if (mod(numero, i) == 0) {
+        return false;
+      }
+    }
+    return true;
   }
 
   public int MDC(int a, int b) {
-    if (b <= a & b > 0) {
-      while (b != 0) {
-        int m = mod(a, b);
-        a = b;
-        b = m;
-      }
-      return a;
+    if (b > a || b <= 0) {
+      throw new IllegalArgumentException("a ou b inválido: a = " + a + ", b = " + b);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: b <= a & b > 0");
+    while (b != 0) {
+      int m = mod(a, b);
+      a = b;
+      b = m;
+    }
+    return a;
   }
 
   public int MDC2(int a, int b) {
-    if (b <= a & b > 0) {
-      while (a != b) {
-        if (a > b) {
-          a -= b;
-        } else {
-          b -= a;
-        }
-      }
-      return a;
+    if (b > a || b <= 0) {
+      throw new IllegalArgumentException("a ou b inválido: a = " + a + ", b = " + b);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: b <= a & b > 0");
+    while (a != b) {
+      if (a > b) {
+        a -= b;
+      } else {
+        b -= a;
+      }
+    }
+    return a;
   }
 
   public int[] crivoEratostenes(int a[]) {
-    if (a.length > 1) {
-      for (int i = 1; i < a.length; i++) {
-        if (a[i] != 0) {
-          throw new IllegalArgumentException("Argumento fora do intervalo permitido: a[" + i + "] != 0 ");
+    if (a.length <= 1) {
+      throw new IllegalArgumentException("a.length inválido: " + a.length);
+    }
+    for (int i = 1; i < a.length; i++) {
+      if (a[i] != 0) {
+        throw new IllegalArgumentException("a[" + i + "] inválido: " + a[i]);
+      }
+    }
+    int limite = (int) Math.floor(raiz(a.length, 100));
+    for (int i = 1; i < limite; i++) {
+      if (a[i] == 0) {
+        for (int multiplo = 2 * i; multiplo < a.length; multiplo += i) {
+          a[multiplo] = 1;
         }
       }
-
-      int limite = (int) Math.floor(raiz(a.length, 100));
-      for (int i = 1; i < limite; i++) {
-        if (a[i] == 0) {
-          for(int multiplo = 2*i; multiplo < a.length; multiplo += i) {
-            a[multiplo] = 1;
-          }
-        }
-      }
-      return a;
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: a.length > 1");
+    return a;
   }
 
-  public int horner(int x, int[] vetorNumeros){
-    if(vetorNumeros.length >= 1){
-      int p = vetorNumeros[vetorNumeros.length-1];
-      for(int i = vetorNumeros.length-2; i >= 0; i--){
-        p = p * x + vetorNumeros[i];
-      }
-      return p;
+  public int horner(int x, int[] vetorNumeros) {
+    if (vetorNumeros.length < 1) {
+      throw new IllegalArgumentException("vetorNumeros.length inválido: " + vetorNumeros.length);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: vetorNumeros.lenght >= 1");
+    int p = vetorNumeros[vetorNumeros.length - 1];
+    for (int i = vetorNumeros.length - 2; i >= 0; i--) {
+      p = p * x + vetorNumeros[i];
+    }
+    return p;
   }
 
-  public int fibonacci(int n){
-    if(n >=0){
-      int a = 0;
-      int c = 1;
-      if(n == 0 | n ==1){
-        return n;
-      }
-      for(int i =2; i<= n; i++){
-        int t = c;
-        c += a;
-        a = t;
-      }
-      return c;
+  public int fibonacci(int n) {
+    if (n < 0) {
+      throw new IllegalArgumentException("n inválido: " + n);
     }
-    throw new IllegalArgumentException("Argumento fora do intervalo permitido: n >= 0");
+    int a = 0;
+    int c = 1;
+    if (n == 0 | n == 1) {
+      return n;
+    }
+    for (int i = 2; i <= n; i++) {
+      int t = c;
+      c += a;
+      a = t;
+    }
+    return c;
   }
 
 }
--- aula-01/js/Matematica.js
+++ aula-01/js/Matematica.js
@@ -1,39 +1,41 @@
 function mod(x, y) {
-  if (y >= 0 && x > 0) {
-    var resto = x;
-    for (; y <= resto; resto -= y);
-    return resto;
+  if (y < 0 || x <= 0) {
+    throw new RangeError("x ou y inválido: x = " + x + ", y = " + y);
   }
-  throw new Error("Argumento fora do intervalo permitido: y >=0 & x > 0");
+  var resto = x;
+  while (resto >= y) {
+    resto = resto - y;
+  }
+  return resto;
 }
 
 function produto(a, b) {
-  if (a >= 0 && b >= 0) {
-    var totalParcelas = a;
-    var parcela = b;
-    if (b < a) {
-      totalParcelas = b;
-      parcela = a;
-    }
-    var soma = 0;
-    for (var i = 1; i <= totalParcelas; i += 1) {
-      soma += parcela;
-    }
-    return soma;
+  if (a < 0 || b < 0) {
+    throw new RangeError("a ou b inválido: a = " + a + ", b = " + b);
   }
-  throw new Error("Argumento fora do intervalo permitido: a>=0 & b>=0");
+  var totalParcelas = a;
+  var parcela = b;
+  if (b < a) {
+    totalParcelas = b;
+    parcela = a;
+  }
+  var soma = 0;
+  for (var i = 1; i <= totalParcelas; i += 1) {
+    soma += parcela;
+  }
+  return soma;
 }
 
 
 function potencia(base, expoente) {
-  if (base >= 0 && expoente >= 0) {
-    var potencia2 = 1;
-    for (var i = 1; i <= expoente; i++) {
-      potencia2 = produto(potencia2, base);
-    }
-    return potencia2;
+  if (base < 0 || expoente < 0) {
+    throw new RangeError("base ou expoente inválido: base = " + base + ", expoente = " + expoente);
   }
-  throw new Error("Argumento fora do intervalo permitido: base>=0 & expoente>=0");
+  var potencia2 = 1;
+  for (var i = 1; i <= expoente; i++) {
+    potencia2 = produto(potencia2, base);
+  }
+  return potencia2;
 }
 
 function intervalo(numero, intervaloInferior, intervaloSuperior) {
@@ -43,197 +45,196 @@ function intervalo(numero, intervaloInferior, intervaloSuperior) {
 }
 
 function propriedade3025(numero) {
-  if (intervalo(numero, 0, 9999)) {
-    var quociente = parseInt(numero / 100);
-    var resto = mod(numero, 100);
-    var resultado = potencia(quociente + resto, 2);
-    return numero === resultado;
+  if (!intervalo(numero, 0, 9999)) {
+    throw new RangeError("numero inválido: " + numero);
   }
-  throw new Error("Argumento fora do intervalo permitido: 0 <= numero <= 9999");
+  var quociente = parseInt(numero / 100);
+  var resto = mod(numero, 100);
+  var resultado = potencia(quociente + resto, 2);
+  return numero === resultado;
 }
 
 function propriedade153(numero) {
-  if (intervalo(numero, 100, 999)) {
-    var quociente = parseInt(numero / 100);
-    var resto = mod(numero, 100);
-    var quociente2 = parseInt(resto / 10);
-    var resto2 = mod(resto, 10);
-    var resultado = potencia(quociente, 3) + potencia(quociente2, 3) + potencia(resto2, 3);
-    return resultado === numero;
+  if (!intervalo(numero, 100, 999)) {
+    throw new RangeError("numero inválido: " + numero);
   }
-  throw new Error("Argumento fora do intervalo permitido: 100 <= numero <= 999");
+  var quociente = parseInt(numero / 100);
+  var resto = mod(numero, 100);
+  var quociente2 = parseInt(resto / 10);
+  var resto2 = mod(resto, 10);
+  var resultado = potencia(quociente, 3) + potencia(quociente2, 3) + potencia(resto2, 3);
+  return resultado === numero;
 }
 
 function somaNaturais(numero) {
-  if (numero >= 1) {
-    var soma = 1;
-    for (var i = 2; numero >= i; i++) {
-      soma += i;
-    }
-    return soma;
+  if (numero < 1) {
+    throw new RangeError("numero inválido: " + numero);
   }
-  throw new Error("Argumento fora do intervalo permitido: numero >= 1");
+  var soma = 1;
+  for (var i = 2; numero >= i; i++) {
+    soma += i;
+  }
+  return soma;
 }
 
 function fatorial(numero) {
-  if (numero >= 1) {
-    var fatorial = 1;
-    for (var i = 2; i <= numero; i++) {
-      fatorial *= i;
-    }
-    return fatorial;
+  if (numero < 1) {
+    throw new RangeError("numero inválido: " + numero);
   }
-  throw new Error("Argumento fora do intervalo permitido: numero >= 1");
+  var fatorial = 1;
+  for (var i = 2; i <= numero; i++) {
+    fatorial *= i;
+  }
+  return fatorial;
 }
 
 function pi(n) {
-  if (n >= 1) {
-    var p = 0, s = -1, d = -1;
-    for (var i = 1; i <= n; i++) {
-      d += 2;
-      s = -1 * s;
-      p += 4 * s / d;
-    }
-    return p;
+  if (n < 1) {
+    throw new RangeError("n inválido: " + n);
   }
-  throw new Error("Argumento fora do intervalo permitido: n >= 1");
+  var p = 0, s = -1, d = -1;
+  for (var i = 1; i <= n; i++) {
+    d += 2;
+    s = -1 * s;
+    p += 4 * s / d;
+  }
+  return p;
 }
 
 function logaritmoNatural(n, k) {
-  if (n >= 1 && k >= 2) {
-    var e = 1 + n;
-    var numerador = n;
-    var denominador = 1;
-    for (var i = 2; i <= k; i++) {
-      numerador *= numerador;
-      denominador += i;
-      e += numerador / denominador;
-    }
-    return e;
+  if (n < 1 || k < 2) {
+    throw new RangeError("n ou k inválido: n = " + n + ", k = " + k);
   }
-  throw new Error("Argumento fora do intervalo permitido: n >= 1 & k >= 2");
+  var e = 1 + n;
+  var numerador = n;
+  var denominador = 1;
+  for (var i = 2; i <= k; i++) {
+    numerador *= numerador;
+    denominador += i;
+    e += numerador / denominador;
+  }
+  return e;
 }
 
 function razaoAurea(x, y, k) {
-  if (x >= 0 && x < y && k > 0) {
-    var c = y;
-    var a = x;
-    for (var i = 1; i <= k; i++) {
-      var t = c;
-      c += a;
-      a = t;
-    }
-    return c / a;
+  if (x < 0 || x >= y || k <= 0) {
+    throw new RangeError("x, y ou k inválido: x = " + x + ", y = " + y + ", k = " + k);
   }
-  throw new Error("Argumento fora do intervalo permitido: x >= 0 & x < y & k > 0");
+  var c = y;
+  var a = x;
+  for (var i = 1; i <= k; i++) {
+    var t = c;
+    c += a;
+    a = t;
+  }
+  return c / a;
 }
 
 
 function quadradoPerfeito(numero) {
-  if (numero >= 1) {
-    var s = 1;
-    for (var i = 3; s < numero; i += 2) {
-      s += i;
-    }
-    return s == numero;
+  if (numero < 1) {
+    throw new RangeError("numero inválido: " + numero);
   }
-  throw new Error("Argumento fora do intervalo permitido: numero >= 1");
+  var s = 1;
+  for (var i = 3; s < numero; i += 2) {
+    s += i;
+  }
+  return s == numero;
 }
 
 function raiz(numero, i) {
-  if (numero > 0) {
-    var r = 1;
-    for (; i >= 0; i--) {
-      r = (r + numero / r) / 2;
-    }
-    return r;
+  if (numero <= 0) {
+    throw new RangeError("numero inválido: " + numero);
   }
-  throw new Error("Argumento fora do intervalo permitido: numero > 0");
+  var r = 1;
+  for (; i >= 0; i--) {
+    r = (r + numero / r) / 2;
+  }
+  return r;
 }
 
 function primo(numero) {
-  if (numero > 1) {
-    for (var i = 2; i < numero; i++) {
-      if (mod(numero, i) == 0) {
-        return false;
-      }
-    }
-    return true;
+  if (numero <= 1) {
+    throw new RangeError("numero inválido: " + numero);
   }
-  throw new Error("Argumento fora do intervalo permitido: numero > 1");
+  for (var i = 2; i < numero; i++) {
+    if (mod(numero, i) == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function MDC(a, b) {
-  if (b <= a && b > 0) {
-    while (b !== 0) {
-      var m = mod(a, b);
-      a = b;
-      b = m;
-    }
-    return a;
+  if (b > a || b <= 0) {
+    throw new RangeError("a ou b inválido: a = " + a + ", b = " + b);
   }
-  throw new Error("Argumento fora do intervalo permitido: b <= a & b > 0");
+  while (b !== 0) {
+    var m = mod(a, b);
+    a = b;
+    b = m;
+  }
+  return a;
 }
 
 function MDC2(a, b) {
-  if (b <= a && b > 0) {
-    while (a !== b) {
-      if (a > b) {
-        a -= b;
-      } else {
-        b -= a;
-      }
-    }
-    return a;
+  if (b > a || b <= 0) {
+    throw new RangeError("a ou b inválido: a = " + a + ", b = " + b);
   }
-  throw new Error("Argumento fora do intervalo permitido: b <= a & b > 0");
+  while (a !== b) {
+    if (a > b) {
+      a -= b;
+    } else {
+      b -= a;
+    }
+  }
+  return a;
 }
 
 function crivoEratostenes(a) {
-  if (a.length > 1) {
-    for (var i = 1; i < a.length; i++) {
-      if (a[i] != 0) {
-        throw new Error("Argumento fora do intervalo permitido: a[" + i + "] != 0 ");
-      }
-    }
-
-    var limite = Math.floor(raiz(a.length, 100));
-    for (var i = 1; i < limite; i++) {
-      if (a[i] == 0) {
-        for (var multiplo = 2 * i; multiplo < a.length; multiplo += i) {
-          a[multiplo] = 1;
-        }
-      }
-    }
-    return a;
+  if (a.length <= 1) {
+    throw new Error("a.length inválido: " + a.length);
   }
-  throw new Error("Argumento fora do intervalo permitido: a.length > 1");
+  for (var i = 1; i < a.length; i++) {
+    if (a[i] != 0) {
+      throw new RangeError("a[" + i + "] inválido: " + a[i]);
+    }
+  }
+  var limite = Math.floor(raiz(a.length, 100));
+  for (var i = 1; i < limite; i++) {
+    if (a[i] == 0) {
+      for (var multiplo = 2 * i; multiplo < a.length; multiplo += i) {
+        a[multiplo] = 1;
+      }
+    }
+  }
+  return a;
 }
 
 function horner(x, vetorNumeros) {
-  if (vetorNumeros.length >= 1) {
-    var p = vetorNumeros[vetorNumeros.length - 1];
-    for (var i = vetorNumeros.length - 2; i >= 0; i--) {
-      p = p * x + vetorNumeros[i];
-    }
-    return p;
+  if (vetorNumeros.length < 1) {
+    throw new Error("vetorNumeros.length inválido: " + vetorNumeros.length);
   }
-  throw new Error("Argumento fora do intervalo permitido: vetorNumeros.lenght >= 1");
+  var p = vetorNumeros[vetorNumeros.length - 1];
+  for (var i = vetorNumeros.length - 2; i >= 0; i--) {
+    p = p * x + vetorNumeros[i];
+  }
+  return p;
 }
 
 function fibonacci(n) {
-  if (n >= 0) {
-    var a = 0;
-    var c = 1;
-    if (n === 0 | n === 1) {
-      return n;
-    }
-    for (var i = 2; i <= n; i++) {
-      var t = c;
-      c += a;
-      a = t;
-    }
-    return c;
+  if (n < 0) {
+    throw new RangeError("n inválido: " + n);
   }
-  throw new Error("Argumento fora do intervalo permitido: n >= 0");
+  var a = 0;
+  var c = 1;
+  if (n === 0 | n === 1) {
+    return n;
+  }
+  for (var i = 2; i <= n; i++) {
+    var t = c;
+    c += a;
+    a = t;
+  }
+  return c;
 }

aaaa9984987203b269b53d4fb9a41a6405d3fe1d
Alice Francener <alicefrancener@gmail.com>
2019-05-07 16:15:01 -0300

    Adiciona testes para undefined/null e tipo de argumento

--- aula-01/js/test/anoValido.test.js
+++ aula-01/js/test/anoValido.test.js
@@ -2,6 +2,24 @@ const funcao = require("../Data.js");
 
 test("ano fora da faixa", () => {
     expect(() => {
-        funcao.f1(350);
+        funcao.f3(350);
+    }).toThrow();
+});
+
+test("ano null/undefined", () => {
+    expect(() => {
+        funcao.f3();
+    }).toThrow();
+});
+
+test("ano nao numerico", () => {
+    expect(() => {
+        funcao.f3("abcd");
+    }).toThrow();
+});
+
+test("ano nao inteiro", () => {
+    expect(() => {
+        funcao.f3(10.5);
     }).toThrow();
 });
--- aula-01/js/test/mesValido.test.js
+++ aula-01/js/test/mesValido.test.js
@@ -5,3 +5,21 @@ test("mes fora da faixa", () => {
         funcao.f2(13);
     }).toThrow();
 });
+
+test("mes null/undefined", () => {
+    expect(() => {
+        funcao.f2();
+    }).toThrow();
+});
+
+test("mes nao numerico", () => {
+    expect(() => {
+        funcao.f2("abcd");
+    }).toThrow();
+});
+
+test("mes nao inteiro", () => {
+    expect(() => {
+        funcao.f2(10.5);
+    }).toThrow();
+});

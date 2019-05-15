8eb55689e93cc96bc59ed5cd70f1d985133c03c8
Alice Francener <alicefrancener@gmail.com>
2019-05-15 18:28:02 -0300

    Adiciona package-info

---
+++ aula-01/java/src/main/java/com/github/alicefng/cs/aula1/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Pacote do repositório de "cs-2019-01". Contém cinco classes:
+ * (a) {@link com.github.alicefng.cs.aula1.domain.CPF},
+ * (b) {@link ccom.github.alicefng.cs.aula1.domain.Data},
+ * (c) {@link ccom.github.alicefng.cs.aula1.domain.DataInvalidaException},
+ * (d) {@link ccom.github.alicefng.cs.aula1.domain.Matematica} e
+ * (e) {@link ccom.github.alicefng.cs.aula1.application.console.Programa}.
+ * Observe que essa última classe apenas ilustra como um
+ * programa em Java pode chamar o código das outras quatro classes.
+ *
+ * @see com.github.alicefng.cs.aula1.domain.CPF
+ * @see com.github.alicefng.cs.aula1.domain.Data
+ * @see com.github.alicefng.cs.aula1.domain.DataInvalidaException
+ * @see com.github.alicefng.cs.aula1.domain.Matematica
+ * @see com.github.alicefng.cs.aula1.application.console.Programa
+ */
+package com.github.alicefng.cs.aula1;

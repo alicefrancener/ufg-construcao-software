aaf7c48bf5137a8104f0bd6ba8e3c5fd639d50a4
Alice Francener <alicefrancener@gmail.com>
2019-05-15 19:02:11 -0300

    Corrige parâmetros final

--- aula-01/java/src/main/java/com/github/alicefng/cs/aula1/domain/Data.java
+++ aula-01/java/src/main/java/com/github/alicefng/cs/aula1/domain/Data.java
@@ -52,7 +52,7 @@ public class Data {
      * @param dia Inteiro que informa dia do mês
      * @throws DataInvalidaException Se o dia for inválido
      */
-    public static void validaDia(int dia) {
+    public static void validaDia(final int dia) {
         if (dia < MENOR_DIA || dia > MAIOR_DIA) {
             throw new DataInvalidaException("dia invalido: " + dia);
         }
@@ -64,7 +64,7 @@ public class Data {
      * @param mes Inteiro que informa mês do ano
      * @throws DataInvalidaException Se o mês for inválido
      */
-    private static void validaMes(int mes) {
+    private static void validaMes(final int mes) {
         if (mes < MENOR_MES || mes > MAIOR_MES) {
             throw new DataInvalidaException("mes inválido: " + mes);
         }
@@ -76,7 +76,7 @@ public class Data {
      * @param ano Inteiro que informa ano
      * @throws DataInvalidaException Se o ano for inválido
      */
-    private static void validaAno(int ano) {
+    private static void validaAno(final int ano) {
         if (ano <= MENOR_ANO) {
             throw new DataInvalidaException("ano inválido: " + ano);
         }
@@ -93,13 +93,13 @@ public class Data {
      * @throws IllegalArgumentException se o dia informado estiver fora da
      *                                  amplitude
      */
-    private static String diaDaSemanaToString(int dia) {
+    private static String diaDaSemanaToString(final int dia) {
         if (dia < 0 || dia > 6) {
             throw new IllegalArgumentException("Dia inválido: " + dia + ". "
                     + "Dia deve estar entre 0 e 6.");
         }
 
-        String nomesDiasDaSemana[] = {"segunda-feira", "terça-feira",
+        final String[] nomesDiasDaSemana = {"segunda-feira", "terça-feira",
                 "quarta-feira", "quinta-feira", "sexta-feira", "sábado",
                 "domingo"};
 
@@ -114,18 +114,21 @@ public class Data {
      * @param ano Inteiro que informa ano
      * @return O nome do dia da semana correspondente a data fornecida
      */
-    public static String diaDaSemana(int dia, int mes, int ano) {
+    public static String diaDaSemana(final int dia, final int mes,
+                                     final int ano) {
         validaDia(dia);
         validaMes(mes);
         validaAno(ano);
 
+        int m = mes;
+        int a = ano;
         if (mes == 1 | mes == 2) {
-            mes = mes + 12;
-            ano = ano - 1;
+            m = m + 12;
+            a = a - 1;
         }
-        int calculoDiaDaSemanaParcial = dia + 2 * mes + 3 * (mes + 1) / 5
-                + ano + ano / 4 - ano / 100 + ano / 400;
-        int calculoDiaDaSemanaFinal = calculoDiaDaSemanaParcial % 7;
+        int calculoDiaDaSemanaParcial = dia + 2 * m + 3 * (m + 1) / 5
+                + a + a / 4 - a / 100 + a / 400;
+        final int calculoDiaDaSemanaFinal = calculoDiaDaSemanaParcial % 7;
 
         return diaDaSemanaToString(calculoDiaDaSemanaFinal);
     }

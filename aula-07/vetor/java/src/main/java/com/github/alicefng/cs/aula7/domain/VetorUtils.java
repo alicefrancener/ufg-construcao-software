1ae99664ebb21bc98fcb206d5195c8aaac67d26b
Alice Francener <alicefrancener@gmail.com>
2019-05-30 20:01:36 -0300

    Configuração inicial do projeto Java

---
+++ aula-07/vetor/java/src/main/java/com/github/alicefng/cs/aula7/domain/VetorUtils.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2019.
+ * Fábrica de Software - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+
+package com.github.alicefng.cs.aula7.domain;
+
+/**
+ * Implementação de várias operações com vetores.
+ */
+public final class VetorUtils {
+
+    /**
+     * Previne a classe utilitária de ser instanciada.
+     */
+    private VetorUtils() {
+        throw new UnsupportedOperationException();
+    }
+    
+}

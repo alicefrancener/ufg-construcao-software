680f4af84fab2c95b5c486a1f3b89b0bebfeb026
Alice Francener <alicefrancener@gmail.com>
2019-06-03 09:29:27 -0300

    Implementa teste para método somaImpares()

--- aula-07/vetor/java/src/test/java/com/github/alicefng/cs/aula7/domain/VetorUtilsTest.java
+++ aula-07/vetor/java/src/test/java/com/github/alicefng/cs/aula7/domain/VetorUtilsTest.java
@@ -20,4 +20,16 @@ public class VetorUtilsTest {
         assertEquals(15.8, VetorUtils.menorTemperatura(vetorTest1));
         assertEquals(-55, VetorUtils.menorTemperatura(vetorTest2));
     }
+
+    @Test
+    public void testSomaImpares() {
+        int[] vetorTest1 = {1};
+        int[] vetorTest2 = {10, 3, 7, 1, 11, 101, 2, 8, 20};
+        int[] vetorTest3 = {2, 8, 10, 6, 8, 22};
+
+        assertEquals(1, VetorUtils.somaImpares(vetorTest1));
+        assertEquals(123, VetorUtils.somaImpares(vetorTest2));
+        assertEquals(0, VetorUtils.somaImpares(vetorTest3));
+    }
+
 }

d080bfe337c898259a091a731d590ab163e482c8
Alice Francener <alicefrancener@gmail.com>
2019-06-04 22:00:27 -0300

    Implementa classe LeituraArquivo

--- aula-08/inteiro/src/main/java/com/github/alicefng/cs/aula8/application/console/Programa.java
+++ aula-08/inteiro/src/main/java/com/github/alicefng/cs/aula8/application/console/Programa.java
@@ -1,4 +1,18 @@
 package com.github.alicefng.cs.aula8.application.console;
 
+import com.github.alicefng.cs.aula8.domain.LeituraArquivo;
+
+import java.io.IOException;
+
 public class Programa {
+
+    public static void main(String[] args) {
+        try {
+            LeituraArquivo.lerArquivo(args[0]);
+            //LeituraArquivo.lerArquivo("C:\\Users\\alice\\Desktop\\test\\LeituraArquivo.class");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- aula-08/inteiro/src/main/java/com/github/alicefng/cs/aula8/domain/LeituraArquivo.java
+++ aula-08/inteiro/src/main/java/com/github/alicefng/cs/aula8/domain/LeituraArquivo.java
@@ -1,4 +1,18 @@
 package com.github.alicefng.cs.aula8.domain;
 
-public class LeituraArquivo {
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public final class LeituraArquivo {
+
+    public static void lerArquivo(String nomeArquivo) throws IOException {
+        FileInputStream fis = new FileInputStream(nomeArquivo);
+        DataInputStream dis = new DataInputStream(fis);
+
+        int valor = dis.readInt();
+        System.out.println(Integer.toHexString(valor));
+
+        dis.close();
+    }
 }

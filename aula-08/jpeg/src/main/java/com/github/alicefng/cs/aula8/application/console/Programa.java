4b45cfcc2b0f2caad544fe71d1dcc8e2ef50cd0a
Alice Francener <alicefrancener@gmail.com>
2019-06-10 17:35:13 -0300

    Imprime resultado final

--- aula-08/jpeg/src/main/java/com/github/alicefng/cs/aula8/application/console/Programa.java
+++ aula-08/jpeg/src/main/java/com/github/alicefng/cs/aula8/application/console/Programa.java
@@ -8,7 +8,7 @@ public class Programa {
 
     public static void main(String[] args) {
         try {
-            IsJpeg.isJpeg(args[0]);
+            System.out.println(IsJpeg.isJpeg(args[0]));
         } catch (IOException e) {
             e.printStackTrace();
         }

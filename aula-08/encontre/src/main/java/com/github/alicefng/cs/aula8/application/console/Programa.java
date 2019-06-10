9237f0533464a6a7f81c1adec41fe71d317ec04c
Alice Francener <alicefrancener@gmail.com>
2019-06-10 19:53:32 -0300

    Configura projeto

---
+++ aula-08/encontre/src/main/java/com/github/alicefng/cs/aula8/application/console/Programa.java
@@ -0,0 +1,11 @@
+package com.github.alicefng.cs.aula8.application.console;
+
+import com.github.alicefng.cs.aula8.domain.EncontraPalavra;
+
+public class Programa {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ aula-08/encontre/src/main/java/com/github/alicefng/cs/aula8/domain/EncontraPalavra.java
@@ -0,0 +1,5 @@
+package com.github.alicefng.cs.aula8.domain;
+
+public final class EncontraPalavra {
+
+}

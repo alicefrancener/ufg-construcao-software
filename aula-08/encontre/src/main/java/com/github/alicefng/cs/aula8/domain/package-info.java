cb4fb1cb90dbe78600b6a03fa90acd84bc2c3026
Alice Francener <alicefrancener@gmail.com>
2019-06-11 16:18:07 -0300

    Inclui informações do pacote

---
+++ aula-08/encontre/src/main/java/com/github/alicefng/cs/aula8/domain/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Pacote do repositório "cs-2019-01". Contém uma classe:
+ * {@link com.github.alicefng.cs.aula8.domain.EncontraPalavra}
+ * @see com.github.alicefng.cs.aula8.domain.EncontraPalavra
+ */
+package com.github.alicefng.cs.aula8.domain;

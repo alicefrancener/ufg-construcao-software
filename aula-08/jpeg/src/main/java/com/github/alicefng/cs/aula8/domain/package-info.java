9debac46d169918fb62821b7d7beb00cc88da7f5
Alice Francener <alicefrancener@gmail.com>
2019-06-11 15:54:57 -0300

    Inclui informações do pacote

---
+++ aula-08/jpeg/src/main/java/com/github/alicefng/cs/aula8/domain/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Pacote do repositório "cs-2019-01". Contém uma classe:
+ * {@link com.github.alicefng.cs.aula8.domain.IsJpeg}
+ * @see com.github.alicefng.cs.aula8.domain.IsJpeg
+ */
+package com.github.alicefng.cs.aula8.domain;

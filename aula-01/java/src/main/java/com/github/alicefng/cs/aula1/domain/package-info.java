d61aa397073e774168125b424a861d328c80deab
Alice Francener <alicefrancener@gmail.com>
2019-06-14 23:07:16 -0300

    Refatora nome classes

--- aula-01/java/src/main/java/com/github/alicefng/cs/aula1/domain/package-info.java
+++ aula-01/java/src/main/java/com/github/alicefng/cs/aula1/domain/package-info.java
@@ -1,17 +1,13 @@
 /**
- * Pacote do repositório de "cs-2019-01". Contém cinco classes:
- * (a) {@link com.github.alicefng.cs.aula1.domain.CPF},
- * (b) {@link com.github.alicefng.cs.aula1.domain.Data},
+ * Pacote do repositório de "cs-2019-01". Contém quatro classes:
+ * (a) {@link com.github.alicefng.cs.aula1.domain.CpfUtils},
+ * (b) {@link com.github.alicefng.cs.aula1.domain.DataUtils},
  * (c) {@link com.github.alicefng.cs.aula1.domain.DataInvalidaException},
- * (d) {@link com.github.alicefng.cs.aula1.domain.Matematica} e
- * (e) {@link com.github.alicefng.cs.aula1.application.console.Programa}.
- * Observe que essa última classe apenas ilustra como um
- * programa em Java pode chamar o código das outras quatro classes.
+ * (d) {@link com.github.alicefng.cs.aula1.domain.MatematicaUtils} e
  *
- * @see com.github.alicefng.cs.aula1.domain.CPF
- * @see com.github.alicefng.cs.aula1.domain.Data
+ * @see com.github.alicefng.cs.aula1.domain.CpfUtils
+ * @see com.github.alicefng.cs.aula1.domain.DataUtils
  * @see com.github.alicefng.cs.aula1.domain.DataInvalidaException
- * @see com.github.alicefng.cs.aula1.domain.Matematica
- * @see com.github.alicefng.cs.aula1.application.console.Programa
+ * @see com.github.alicefng.cs.aula1.domain.MatematicaUtils
  */
-package com.github.alicefng.cs.aula1;
+package com.github.alicefng.cs.aula1.domain;

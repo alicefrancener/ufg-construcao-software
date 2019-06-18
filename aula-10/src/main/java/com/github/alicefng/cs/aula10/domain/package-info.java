7e85c39e412c2272bd0fb9b69c7eb46e4e258167
Alice Francener <alicefrancener@gmail.com>
2019-06-18 11:59:26 -0300

    Documenta pacote

---
+++ aula-10/src/main/java/com/github/alicefng/cs/aula10/domain/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Pacote do repositório "cs-2019-01". Contém as seguintes classes:
+ * {@link com.github.alicefng.cs.aula10.domain.ValidaEntradaUtils}.
+ * @see com.github.alicefng.cs.aula10.domain.ValidaEntradaUtils
+ */
+package com.github.alicefng.cs.aula10.domain;

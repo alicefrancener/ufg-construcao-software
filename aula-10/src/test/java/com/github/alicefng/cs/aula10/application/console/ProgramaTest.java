16543fb6951b14f400f7389bafdf7f2b3720c795
Alice Francener <alicefrancener@gmail.com>
2019-06-18 12:01:33 -0300

    Implementa testes

---
+++ aula-10/src/test/java/com/github/alicefng/cs/aula10/application/console/ProgramaTest.java
@@ -0,0 +1,13 @@
+package com.github.alicefng.cs.aula10.application.console;
+
+import org.junit.jupiter.api.Test;
+
+public class ProgramaTest {
+
+    @Test
+    public void testMain() {
+        String[] args = {"20190618", "2019", "20190617", "1"};
+        Programa.main(args);
+    }
+
+}
---
+++ aula-10/src/test/java/com/github/alicefng/cs/aula10/domain/ValidaEntradaUtilsTest.java
@@ -0,0 +1,33 @@
+package com.github.alicefng.cs.aula10.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ValidaEntradaUtilsTest {
+
+    @Test
+    public void testQtdArgumentos() {
+        String[] test1 = {};
+        assertEquals(-1, ValidaEntradaUtils.qtdArgumentos(test1));
+
+        String[] test2 = {"1", "2"};
+        assertEquals(-1, ValidaEntradaUtils.qtdArgumentos(test2));
+
+        String[] test3 = {"1", "2", "3", "4"};
+        assertEquals(0, ValidaEntradaUtils.qtdArgumentos(test3));
+    }
+
+    @Test
+    public void testIsInteiro() {
+        String[] test1 = {"20190618", "2011", "20190618", "1"};
+        assertEquals(0, ValidaEntradaUtils.argumentoIsInteiro(test1));
+
+        String[] test2 = {"alice", "2011", "20190618", "1"};
+        assertEquals(-1, ValidaEntradaUtils.argumentoIsInteiro(test2));
+
+        String[] test3 = {"20190618", "10.5", "20190618", "1"};
+        assertEquals(-1, ValidaEntradaUtils.argumentoIsInteiro(test3));
+    }
+
+}

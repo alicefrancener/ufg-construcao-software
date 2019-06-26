47da7dbf2f1b6e782300337b8cbb7fc84e181d7c
Alice Francener <alicefrancener@gmail.com>
2019-06-26 00:18:57 -0300

    Modifica testes

--- aula-10/src/test/java/com/github/alicefng/cs/aula10/domain/InputUtilsTest.java
+++ aula-10/src/test/java/com/github/alicefng/cs/aula10/domain/InputUtilsTest.java
@@ -2,44 +2,45 @@ package com.github.alicefng.cs.aula10.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InputUtilsTest {
 
     @Test
     public void testQtdArgumentos() {
         String[] test1 = {};
-        assertEquals(-1, InputUtils.qtdArgumentos(test1));
+        assertThrows(IllegalArgumentException.class,
+                () -> InputUtils.qtdArgumentos(test1));
 
         String[] test2 = {"1", "2"};
-        assertEquals(-1, InputUtils.qtdArgumentos(test2));
+        assertThrows(IllegalArgumentException.class,
+                () -> InputUtils.qtdArgumentos(test2));
 
-        String[] test3 = {"1", "2", "3", "4"};
-        assertEquals(0, InputUtils.qtdArgumentos(test3));
+        String[] test3 = {"1", "2", "3"};
+        assertThrows(IllegalArgumentException.class,
+                () -> InputUtils.qtdArgumentos(test3));
     }
 
     @Test
     public void testIsInteiro() {
-        String[] test1 = {"20190618", "2011", "20190618", "1"};
-        assertEquals(0, InputUtils.argumentoIsNatural(test1));
+        String[] test1 = {"20190618", "10.5", "20190618", "1"};
+        assertThrows(NumberFormatException.class,
+                () -> InputUtils.argumentoIsNatural(test1));
 
         String[] test2 = {"alice", "2011", "20190618", "1"};
-        assertEquals(-1, InputUtils.argumentoIsNatural(test2));
-
-        String[] test3 = {"20190618", "10.5", "20190618", "1"};
-        assertEquals(-1, InputUtils.argumentoIsNatural(test3));
+        assertThrows(NumberFormatException.class,
+                () -> InputUtils.argumentoIsNatural(test2));
     }
 
     @Test
     public void testIsNumeroPositivo() {
         String[] test1 = {"-20190618", "2011", "20190618", "1"};
-        assertEquals(-1, InputUtils.argumentoIsNatural(test1));
+        assertThrows(IllegalArgumentException.class,
+                () -> InputUtils.argumentoIsNatural(test1));
 
         String[] test2 = {"20190618", "2011", "20190618", "-1"};
-        assertEquals(-1, InputUtils.argumentoIsNatural(test2));
-
-        String[] test3 = {"20190618", "10", "20190618", "1"};
-        assertEquals(0, InputUtils.argumentoIsNatural(test3));
+        assertThrows(IllegalArgumentException.class,
+                () -> InputUtils.argumentoIsNatural(test2));
     }
 
 }

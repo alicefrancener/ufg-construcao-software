c195c57a6848052267c82667c9ca547604f09d8c
Alice Francener <alicefrancener@gmail.com>
2019-06-26 17:15:53 -0300

    Inclui constantes para cada índice

--- aula-10/src/main/java/com/github/alicefng/cs/aula10/domain/ControladorUtils.java
+++ aula-10/src/main/java/com/github/alicefng/cs/aula10/domain/ControladorUtils.java
@@ -3,6 +3,28 @@ package com.github.alicefng.cs.aula10.domain;
 public final class ControladorUtils {
 
     /**
+     * Índice da data de interesse no parâmetro String[] fornecido.
+     */
+    private static final int INDEX_DATA_INTERESSE = 0;
+
+    /**
+     * Índice do ano bissexto de referência no parâmetro String[] fornecido.
+     */
+    private static final int INDEX_ANO_BISSEXTO = 1;
+
+    /**
+     * Índice da data de referência no parâmetro String[] fornecido.
+     */
+    private static final int INDEX_DATA_REFERENCIA = 2;
+
+    /**
+     * Índice do dia da semana da data de referência no parâmetro String[]
+     * fornecido.
+     */
+    private static final int INDEX_DIA_DA_SEMANA = 3;
+
+
+    /**
      * Construtor privado para evitar instaciação da classe utilitária.
      */
     private ControladorUtils() {
@@ -11,30 +33,32 @@ public final class ControladorUtils {
 
     /**
      * Executa método para obter dia da semana, avaliando todos os parâmetros
-     * de  entrada fornecidos pelo usuário.
+     * de  entrada fornecidos.
      *
      * @param argumentos Argumentos a serem fornecidos pelo usuário
      * @return -1, caso os argumentos fornecidos não estejam de acordo com
      * especificado pelos requisitos; 0 a 6, caso os parâmetros estejam
      * corretos, sendo 0 correspondente à segunda-feira, 1, terça-feira, e
      * assim sucessivamente.
-     * @implNote Para atender aos requisitos R6 e R14
+     * @implNote Para atender aos requisitos R1, R2, R3, R4, R5, R6 e R14
      */
     public static int executar(final String[] argumentos) {
         try {
             InputUtils.evalQtdArgumentos(argumentos);
             InputUtils.evalArgumentoIsNatural(argumentos);
 
-            DataUtils.evalQtdDigitosData(argumentos[0]);
-            DataUtils.evalQtdDigitosData(argumentos[2]);
+            DataUtils.evalQtdDigitosData(argumentos[INDEX_DATA_INTERESSE]);
+            DataUtils.evalQtdDigitosData(argumentos[INDEX_DATA_REFERENCIA]);
 
-            DataUtils.evalRangeAno(argumentos[0]);
-            DataUtils.evalRangeAno(argumentos[2]);
-            DataUtils.evalRangeAnoBissexto(argumentos[1]);
-            DataUtils.evalRangeDiaDaSemana(argumentos[3]);
+            DataUtils.evalRangeAno(argumentos[INDEX_DATA_INTERESSE]);
+            DataUtils.evalRangeAno(argumentos[INDEX_DATA_REFERENCIA]);
+            DataUtils.evalRangeAnoBissexto(argumentos[INDEX_ANO_BISSEXTO]);
+            DataUtils.evalRangeDiaDaSemana(argumentos[INDEX_DIA_DA_SEMANA]);
 
-            return DataUtils.getDiaDaSemana(argumentos[0], argumentos[1],
-                    argumentos[2], argumentos[3]);
+            return DataUtils.getDiaDaSemana(argumentos[INDEX_DATA_INTERESSE],
+                    argumentos[INDEX_ANO_BISSEXTO],
+                    argumentos[INDEX_DATA_REFERENCIA],
+                    argumentos[INDEX_DIA_DA_SEMANA]);
 
         } catch (IllegalArgumentException | DataInvalidaException ex) {
             return -1;

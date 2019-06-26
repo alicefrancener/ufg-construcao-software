b5aae2e6f1d84a4509a9c2f931d57d4dfd95cf2a
Alice Francener <alicefrancener@gmail.com>
2019-06-26 00:06:34 -0300

    Cria exceção

---
+++ aula-10/src/main/java/com/github/alicefng/cs/aula10/domain/DataInvalidaException.java
@@ -0,0 +1,26 @@
+package com.github.alicefng.cs.aula10.domain;
+
+/**
+ * Exceção para datas inválidas.
+ */
+public class DataInvalidaException extends RuntimeException {
+
+      /**
+     * Constroi uma exceção somente com mensagem.
+     *
+     * @param mensagem A mensagem a ser enviada quanto a exceção é lançada.
+     */
+    public DataInvalidaException(final String mensagem) {
+        super(mensagem);
+    }
+
+    /**
+     * Constroi uma exceção com mensagem e causa.
+     *
+     * @param mensagem A mensagem a ser enviada quanto a exceção é lançada.
+     * @param causa Causa raiz da exceção (root case)
+     */
+    public DataInvalidaException(final String mensagem, final Throwable causa) {
+        super(mensagem, causa);
+    }
+}

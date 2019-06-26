14c83f49f3a09860cc9a243133a4feae755dce98
Alice Francener <alicefrancener@gmail.com>
2019-06-26 14:23:40 -0300

    Documenta métodos e constantes

--- aula-10/src/main/java/com/github/alicefng/cs/aula10/domain/DataUtils.java
+++ aula-10/src/main/java/com/github/alicefng/cs/aula10/domain/DataUtils.java
@@ -13,13 +13,13 @@ public final class DataUtils {
     private static final int NUMERO_DIGITOS = 8;
 
     /**
-     * Inteiro correspondente ao mês de janeiro
+     * Inteiro correspondente ao mês de janeiro.
      */
     private static final int JANEIRO = 1;
 
 
     /**
-     * Inteiro correspondente ao mês de fevereiro
+     * Inteiro correspondente ao mês de fevereiro.
      */
     private static final int FEVEREIRO = 2;
 
@@ -97,11 +97,12 @@ public final class DataUtils {
     }
 
     /**
-     * Avalia se quantidade de dígitos da data está correta (R13a).
+     * Avalia se a quantidade de dígitos de uma data está incorreta.
      *
-     * @param data Data no formato (aaaammdd)
-     * @return 0, se a quantidade de dígitos estiver correta; -1, caso não
-     * esteja correta
+     * @param data Data a ser avaliada
+     * @throws DataInvalidaException Se a quantidade de dígitos não for formada
+     *                               exatamente po 8 dígitos
+     * @implNote Para atender o requisito R13a
      */
     public static void qtdDigitosData(final String data) {
         if (data.length() != NUMERO_DIGITOS) {
@@ -109,13 +110,28 @@ public final class DataUtils {
         }
     }
 
+    /**
+     * Avalia se o primeiro dígito da data está incorreto.
+     *
+     * @param data Data a ser avaliada
+     * @throws DataInvalidaException Se o primeiro dígito da data for 0
+     * @implNote Para atender o requisito R13b
+     */
     public static void rangeAno(final String data) {
-        final int primeiroDigitoData = Integer.parseInt(data.substring(0, 1));
+        final int primeiroDigitoData =
+                Integer.parseInt(data.substring(0, 1));
         if (primeiroDigitoData == 0) {
             throw new DataInvalidaException();
         }
     }
 
+    /**
+     * Avalia se amplitude do dia da semana está correta.
+     *
+     * @param diaDaSemana Data a ser avaliada
+     * @throws IllegalArgumentException Se o dia da semana for maior que 6
+     * @implNote Para atender o requisito R10
+     */
     public static void rangeDiaDaSemana(final String diaDaSemana) {
         final int diaDaSemanaAux = Integer.parseInt(diaDaSemana);
         if (diaDaSemanaAux > 6) {
@@ -123,6 +139,13 @@ public final class DataUtils {
         }
     }
 
+    /**
+     * Avalia se amplitude do ano bissexto está correta.
+     *
+     * @param anoBissexto O ano bissexto a ser avaliado
+     * @throws IllegalArgumentException Se o ano bissexto for menor que 1
+     * @implNote Para atender o requisito R11
+     */
     public static void rangeAnoBissexto(final String anoBissexto) {
         final int anoBissextoAux = Integer.parseInt(anoBissexto);
         if (anoBissextoAux < 1) {
@@ -130,18 +153,25 @@ public final class DataUtils {
         }
     }
 
-
+    /**
+     * Obtém o ano como inteiro a partir de uma data no formato "aaaammdd".
+     *
+     * @param data A data da qual se quer obter o ano
+     * @return O ano como inteiro
+     */
     public static int getAnoAsInt(final String data) {
         final int ano = Integer.parseInt(data.substring(0, 4));
         return ano;
     }
 
-
     /**
-     * Avalia mês quanto a sua validade (R13c).
+     * Obtém o mês como inteiro a partir de uma data no formato "aaaammdd".
      *
-     * @param data Mês a ser avaliado
-     * @return -1, se o mês é inválido; 0, caso seja válido
+     * @param data A data da qual se quer obter o mês
+     * @return O mes como inteiro
+     * @throws DataInvalidaException Se o mes for zero ou maior que 12
+     *                               (equivalente ao mês DEZEMBRO)
+     * @implNote Para atender o requisito R13c
      */
     public static int getMesAsInt(final String data) {
         final int mes = Integer.parseInt(data.substring(4, 6));
@@ -153,10 +183,14 @@ public final class DataUtils {
     }
 
     /**
-     * Avalia dia quanto a sua validade (R13d).
+     * Obtém o dia como inteiro a partir de uma data no formato "aaaammdd".
      *
-     * @param data Dia a ser avaliado
-     * @return -1, se o mês é inválido; 0, caso seja válido
+     * @param data        A data da qual se quer obter o dia
+     * @param anoBissexto O ano bissexto de referência
+     * @return O dia como inteiro
+     * @throws DataInvalidaException Se o dia for zero ou maior que 31;
+     *                               Se o dia não for compatível com o mês
+     * @implNote Para atender os requisitos R13d e R13e
      */
     public static int getDiaAsInt(final String data, final String anoBissexto) {
         final int dia = Integer.parseInt(data.substring(6, 8));
@@ -187,6 +221,15 @@ public final class DataUtils {
         return dia;
     }
 
+    /**
+     * Compara duas datas em relação à ordem temporal.
+     *
+     * @param data             A data a ser comparada
+     * @param dataDeReferencia A data de referência
+     * @param anoBissexto      O ano bissexto de referência
+     * @return 0, se data e dataDeReferencia forem iguais; -1, se data for
+     * anterior à dataDeReferencia; 1, se data for posterior à dataDeReferencia
+     */
     public static int comparaDatas(final String data,
                                    final String dataDeReferencia,
                                    final String anoBissexto) {
@@ -220,6 +263,19 @@ public final class DataUtils {
         return 0;
     }
 
+    /**
+     * Obtém o dia da semana de uma data de interesse com base em uma data de
+     * referência e o dia da semana dessa data, além do ano bissexto de
+     * referência.
+     *
+     * @param dataDeInteresse  A data da qual quer se obter o dia da semana
+     * @param anoBissexto      O ano bissexto de referência
+     * @param dataDeReferencia A data de referência
+     * @param diaDaSemana      O dia da semana da data de referência
+     * @return O dia da semana da data de interesse (0 a 6). Com 0
+     * correspondente à segunda-feira, 1 à terça-feira, e assim sucessivamente.
+     * @implNote Para atender requisitos R17 e R18
+     */
     public static final int getDiaDaSemana(final String dataDeInteresse,
                                            final String anoBissexto,
                                            final String dataDeReferencia,
@@ -247,7 +303,13 @@ public final class DataUtils {
         return diaDaSemanaAux;
     }
 
-
+    /**
+     * Adiciona um dia à data passada como argumento.
+     *
+     * @param data        A data a ser adicionada
+     * @param anoBissexto O ano bissexto de referência
+     * @return A data posterior à data passada como argumento
+     */
     public static String adicionaDia(String data, String anoBissexto) {
         int ano = getAnoAsInt(data);
         int mes = getMesAsInt(data);
@@ -306,6 +368,13 @@ public final class DataUtils {
         return String.format("%04d%02d%02d", ano, mes, dia);
     }
 
+    /**
+     * Subtrai um dia à data passada como argumento.
+     *
+     * @param data        A data a ser subtraída
+     * @param anoBissexto O ano bissexto de referência
+     * @return A data anterior à data passada como argumento
+     */
     private static String subtraiDia(String data, String anoBissexto) {
         int ano = getAnoAsInt(data);
         int mes = getMesAsInt(data);
@@ -363,6 +432,12 @@ public final class DataUtils {
         return String.format("%04d%02d%02d", ano, mes, dia);
     }
 
+    /**
+     * Adiciona um mês ao mês passado como argumento.
+     *
+     * @param mes O mês a ser adicionado
+     * @return O mes posterior ao mês passado como argumento
+     */
     private static int adicionaMes(int mes) {
         if (mes == DEZEMBRO) {
             return JANEIRO;
@@ -371,6 +446,12 @@ public final class DataUtils {
         return ++mes;
     }
 
+    /**
+     * Subtrai um mês ao mês passado como argumento.
+     *
+     * @param mes O mês a ser subtraído
+     * @return O mes anterior ao mes passado como argumento
+     */
     private static int subtraiMes(int mes) {
         if (mes == JANEIRO) {
             return DEZEMBRO;
@@ -379,6 +460,12 @@ public final class DataUtils {
         return --mes;
     }
 
+    /**
+     * Adiciona um dia ao dia da semana passado como argumento.
+     *
+     * @param diaDaSemana O dia da semana a ser adicionado
+     * @return O diaDaSemana posterior ao dia da semana passado como argumento
+     */
     private static int adicionaDiaDaSemana(final int diaDaSemana) {
         int diaDaSemanaAux = diaDaSemana;
         if (diaDaSemanaAux == 6) {
@@ -388,6 +475,12 @@ public final class DataUtils {
         return ++diaDaSemanaAux;
     }
 
+    /**
+     * Subtrai um dia do dia da semana passado como argumento.
+     *
+     * @param diaDaSemana O dia da semana a ser subtraído
+     * @return O diaDaSemana anterior ao dia da semana passado
+     */
     private static int subtraiDiaDaSemana(int diaDaSemana) {
         if (diaDaSemana == 0) {
             return 6;
@@ -395,10 +488,14 @@ public final class DataUtils {
         return --diaDaSemana;
     }
 
-    public static boolean isMultiplo(final int num1, final int num2) {
-        return !(num1 % num2 != 0);
-    }
-
+    /**
+     * Avalia se um ano é bissexto dado um ano bissexto de referência.
+     *
+     * @param anoBissexto Ano bissexto de referência
+     * @param anoAvaliado Ano a ser avaliado
+     * @return true, se o ano avaliado é bissexto; false, caso não seja
+     * @implNote Para atender os requisitos R15, R16
+     */
     public static boolean isBissexto(final String anoBissexto,
                                      final int anoAvaliado) {
         final int anoBissextoInt = Integer.parseInt(anoBissexto);
@@ -411,6 +508,15 @@ public final class DataUtils {
         return false;
     }
 
+    /**
+     * Avalia se dois números são múltiplos entre si.
+     *
+     * @param num1 O número1 a ser avaliado
+     * @param num2 O número2 a ser avaliado
+     * @return true, se os números são múltiplos; false, se não são múltiplos
+     */
+    public static boolean isMultiplo(final int num1, final int num2) {
+        return !(num1 % num2 != 0);
+    }
+
 }
-
-

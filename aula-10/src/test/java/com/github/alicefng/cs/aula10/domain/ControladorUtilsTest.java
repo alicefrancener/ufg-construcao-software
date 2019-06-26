8a7eca6fd3f6160fd9ee2761d1bfa975056b2c8a
Alice Francener <alicefrancener@gmail.com>
2019-06-26 10:44:11 -0300

    Implementa testes entradas inválidas

---
+++ aula-10/src/test/java/com/github/alicefng/cs/aula10/domain/ControladorUtilsTest.java
@@ -0,0 +1,38 @@
+package com.github.alicefng.cs.aula10.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ControladorUtilsTest {
+
+    @Test
+    public void dataInvalida() {
+        String[] entrada = {"2010101", "1", "23450101", "1"};
+        String[] entrada2 = {"100000101", "1", "23450101", "1"};
+        String[] entrada3 = {"20161301", "1", "23450101", "1"};
+        String[] entrada4 = {"20160001", "1", "23450101", "1"};
+        String[] entrada5 = {"20160931", "1", "23450101", "1"};
+        String[] entrada6 = {"20160900", "1", "23450101", "1"};
+        String[] entrada7 = {"01110101", "1", "23450101", "1"};
+
+        assertEquals(-1, ControladorUtils.executar(entrada));
+        assertEquals(-1, ControladorUtils.executar(entrada2));
+        assertEquals(-1, ControladorUtils.executar(entrada3));
+        assertEquals(-1, ControladorUtils.executar(entrada4));
+        assertEquals(-1, ControladorUtils.executar(entrada5));
+        assertEquals(-1, ControladorUtils.executar(entrada6));
+        assertEquals(-1, ControladorUtils.executar(entrada7));
+    }
+
+    @Test
+    public void anoBissextoInvalido() {
+        String[] entrada1 = {"20190626", "0", "23450101", "1"};
+        String[] entrada2 = {"20190626", "-100", "23450101", "1"};
+        assertEquals(-1, ControladorUtils.executar(entrada1));
+        assertEquals(-1, ControladorUtils.executar(entrada2));
+    }
+
+}

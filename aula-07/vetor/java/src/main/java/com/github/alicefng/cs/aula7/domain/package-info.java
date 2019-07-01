7c0410788452e17752784dd67273938bfc0d21b7
Alice Francener <alicefrancener@gmail.com>
2019-07-01 17:45:17 -0300

    Insere informações pacote

---
+++ aula-07/vetor/java/src/main/java/com/github/alicefng/cs/aula7/domain/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Pacote do repositório "cs-2019-01". Contém uma classe:
+ * {@link com.github.alicefng.cs.aula7.domain.VetorUtils}
+ * @see com.github.alicefng.cs.aula7.domain.VetorUtils
+ */
+package com.github.alicefng.cs.aula7.domain;

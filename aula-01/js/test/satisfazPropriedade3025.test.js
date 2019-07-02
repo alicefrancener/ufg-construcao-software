70330ba0a9f4eff177dab051ee838dc5200f1a4e
Alice Francener <alicefrancener@gmail.com>
2019-07-01 21:44:37 -0300

    Refatora métodos

--- aula-01/js/test/satisfazPropriedade3025.test.js
+++ aula-01/js/test/satisfazPropriedade3025.test.js
@@ -1,31 +1,31 @@
 const funcao = require("../funcoes/Matematica.js");
 
 test("3025 satisfaz (caso classico)", () => {
-    expect(funcao.possuiPropriedade3025(3025)).toBe(true);
+    expect(funcao.satisfazPropriedade3025(3025)).toBe(true);
 });
 
 test("3024 nao satisfaz", () => {
-    expect(funcao.possuiPropriedade3025(3024)).toBe(false);
+    expect(funcao.satisfazPropriedade3025(3024)).toBe(false);
 });
 
 test("argumento null/undefined", () => {
     expect(() => {
-        funcao.possuiPropriedade3025();
+        funcao.satisfazPropriedade3025();
     }).toThrow(TypeError);
 });
 
 test("argumento nao numerico", () => {
     expect(() => {
-        funcao.possuiPropriedade3025("abcd");
+        funcao.satisfazPropriedade3025("abcd");
     }).toThrow(TypeError);
 });
 
 test("valor negativo fora da faixa", () => {
     expect(() => {
-        funcao.possuiPropriedade3025(-1);
+        funcao.satisfazPropriedade3025(-1);
     }).toThrow(RangeError);
 });
 
 test("valor com mais de 4 digitos fora da faixa", () => {
-    expect(() => funcao.possuiPropriedade3025(10000)).toThrow(RangeError);
+    expect(() => funcao.satisfazPropriedade3025(10000)).toThrow(RangeError);
 });

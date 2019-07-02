d1bc0c7704af38ff2f4d1a8d168fff242eadd6be
Alice Francener <alicefrancener@gmail.com>
2019-07-01 22:45:04 -0300

    Remove comentário

--- aula-07/vetor/js/funcoes/conteIguais.js
+++ aula-07/vetor/js/funcoes/conteIguais.js
@@ -23,16 +23,6 @@ function conteIguais(numeros, comparador) {
         }
     }
 
-    //let quantidade = 0;
-    //for (let i = 0; i < numeros.length; i++) {
-    //    if (numeros[i] === comparador) {
-    //        quantidade++;
-    //    }
-    //}
-
-    //return quantidade;
-    
-    // TODO observe a diferença da proposta acima. Aquela abaixo segue o estilo funcional. 
     return numeros.filter(i => i == comparador).length;
 }
 

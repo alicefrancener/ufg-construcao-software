a85b58197184890232d6bce83300c0b7be8a89fe
Alice Francener <alicefrancener@gmail.com>
2019-07-02 20:05:21 -0300

    Configura projeto inicial

---
+++ aula-11/src/main/java/com/github/alicefng/cs/aula11/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Pacote do repositório de "exemplo". Contém duas classes
+ * apenas: (a) {@link com.github.alicefng.cs.aula11.domain.Calendario} e
+ * (b) {@link com.github.alicefng.cs.aula11.application.console.ProgramaCalendario}.
+ * Observe que essa última classe apenas ilustra como um
+ * programa em Java pode chamar o código da classe
+ * relevante ({@link com.github.alicefng.cs.aula11.domain.Calendario}).
+ *
+ * @see com.github.alicefng.cs.aula11.domain.Calendario
+ * @see com.github.alicefng.cs.aula11.application.console.ProgramaCalendario
+ */
+package com.github.alicefng.cs.aula11;

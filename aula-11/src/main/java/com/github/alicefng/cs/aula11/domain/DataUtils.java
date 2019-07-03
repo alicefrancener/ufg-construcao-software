0218746950b2dcee52d9f993d42491b2ee315836
Alice Francener <alicefrancener@gmail.com>
2019-07-02 21:40:35 -0300

    Cria classe para calcular diferença entre datas

---
+++ aula-11/src/main/java/com/github/alicefng/cs/aula11/domain/DataUtils.java
@@ -0,0 +1,11 @@
+package com.github.alicefng.cs.aula11.domain;
+
+import java.time.LocalDate;
+
+public class DataUtils {
+
+    public static int getDiaEntreDatas(final LocalDate dataInicial,
+                                       final LocalDate dataFinal) {
+        return 0;
+    }
+}

2bfe95fb40569e7dd87429ca4ab5b619b2607df6
Alice Francener <alicefrancener@gmail.com>
2019-07-03 11:28:00 -0300

    Refatora método

--- aula-11/src/main/java/com/github/alicefng/cs/aula11/domain/DataUtils.java
+++ aula-11/src/main/java/com/github/alicefng/cs/aula11/domain/DataUtils.java
@@ -1,15 +1,13 @@
 package com.github.alicefng.cs.aula11.domain;
 
 import java.time.LocalDate;
-import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class DataUtils {
 
-    public static int getDiferencaEntreDatas(final LocalDate dataInicial,
+    public static long getDiferencaEntreDatas(final LocalDate dataInicial,
                                              final LocalDate dataFinal) {
-        Period periodo = Period.between(dataInicial, dataFinal);
-        int diferenca = periodo.getDays();
 
-        return diferenca;
+        return ChronoUnit.DAYS.between(dataInicial, dataFinal);
     }
 }

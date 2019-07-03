ab819b444068ff7f8a67cd8755ca5e313e562179
Alice Francener <alicefrancener@gmail.com>
2019-07-03 11:29:54 -0300

    Implementa teste

---
+++ aula-11/src/test/java/com/github/alicefng/cs/aula11/domain/DataUtilsTest.java
@@ -0,0 +1,36 @@
+package com.github.alicefng.cs.aula11.domain;
+
+import com.github.alicefng.cs.aula11.application.api.DiaDaSemanaController;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DataUtilsTest {
+
+    @Test
+    public void testDiferencaEntreDatas() {
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dataI =  LocalDate.parse("03/07/2019", fmt);
+        LocalDate dataF =  LocalDate.parse("04/07/2019", fmt);
+
+        assertEquals(1, DataUtils.getDiferencaEntreDatas(dataI,dataF));
+        assertEquals(-1, DataUtils.getDiferencaEntreDatas(dataF,dataI));
+        assertEquals(0, DataUtils.getDiferencaEntreDatas(dataI,dataI));
+    }
+
+    @Test
+    public void testDiferencaEntreDatas2() {
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dataI =  LocalDate.parse("03/07/2019", fmt);
+        LocalDate dataF =  LocalDate.parse("02/07/2020", fmt);
+
+        assertEquals(365, DataUtils.getDiferencaEntreDatas(dataI,dataF));
+    }
+
+}

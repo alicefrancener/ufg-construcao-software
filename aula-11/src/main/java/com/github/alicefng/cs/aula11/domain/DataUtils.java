eb4c082d9450321e033482de65ba5cd6e3d6e207
Alice Francener <alicefrancener@gmail.com>
2019-07-02 21:48:55 -0300

    Implementa método

--- aula-11/src/main/java/com/github/alicefng/cs/aula11/domain/DataUtils.java
+++ aula-11/src/main/java/com/github/alicefng/cs/aula11/domain/DataUtils.java
@@ -1,11 +1,15 @@
 package com.github.alicefng.cs.aula11.domain;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class DataUtils {
 
-    public static int getDiaEntreDatas(final LocalDate dataInicial,
-                                       final LocalDate dataFinal) {
-        return 0;
+    public static int getDiferencaEntreDatas(final LocalDate dataInicial,
+                                             final LocalDate dataFinal) {
+        Period periodo = Period.between(dataInicial, dataFinal);
+        int diferenca = periodo.getDays();
+
+        return diferenca;
     }
 }

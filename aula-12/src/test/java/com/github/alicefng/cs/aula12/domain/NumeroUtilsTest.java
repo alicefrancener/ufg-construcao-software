70421a94793a42d43650f79dafb486aef5e78506
Alice Francener <alicefrancener@gmail.com>
2019-07-11 19:32:52 -0300

    Implementa teste

--- aula-12/src/test/java/com/github/alicefng/cs/aula12/domain/NumeroUtilsTest.java
+++ aula-12/src/test/java/com/github/alicefng/cs/aula12/domain/NumeroUtilsTest.java
@@ -3,6 +3,7 @@ package com.github.alicefng.cs.aula12.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public final class NumeroUtilsTest {
 
@@ -12,6 +13,13 @@ public final class NumeroUtilsTest {
                 () -> NumeroUtils.getGrafiaNumeroCardinal(-10));
         assertThrows(IllegalArgumentException.class,
                 () -> NumeroUtils.getGrafiaNumeroCardinal(10000));
+    }
+
+    @Test
+    public  void testGetMilhar() {
+        assertEquals("mil", NumeroUtils.getGrafiaMillhar(1));
+        assertEquals("três mil", NumeroUtils.getGrafiaMillhar(3));
+        assertEquals("nove mil", NumeroUtils.getGrafiaMillhar(9));
     }
 
 }

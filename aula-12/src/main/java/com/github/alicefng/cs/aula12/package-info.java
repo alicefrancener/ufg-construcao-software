813b6ea47cb9bc4eea35011c5c6249fc0186bf76
Alice Francener <alicefrancener@gmail.com>
2019-07-12 10:31:30 -0300

    Inclui package-info

---
+++ aula-12/src/main/java/com/github/alicefng/cs/aula12/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Pacote do repositório "aula-12". Contém uma classe:
+ * (a) {@link com.github.alicefng.cs.aula12.domain.NumeroUtils}
+ * @see com.github.alicefng.cs.aula12.domain.NumeroUtils
+ */
+package com.github.alicefng.cs.aula12;

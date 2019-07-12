69eda0fd4c4157e3e5980f92bb02fed947ab2a25
Alice Francener <alicefrancener@gmail.com>
2019-07-11 21:19:32 -0300

    Implementa testes

--- aula-12/src/test/java/com/github/alicefng/cs/aula12/domain/NumeroUtilsTest.java
+++ aula-12/src/test/java/com/github/alicefng/cs/aula12/domain/NumeroUtilsTest.java
@@ -16,10 +16,47 @@ public final class NumeroUtilsTest {
     }
 
     @Test
-    public  void testGetMilhar() {
+    public void testGetMilhar() {
         assertEquals("mil", NumeroUtils.getGrafiaMillhar(1));
         assertEquals("três mil", NumeroUtils.getGrafiaMillhar(3));
         assertEquals("nove mil", NumeroUtils.getGrafiaMillhar(9));
+    }
+
+    @Test
+    public void testGetCentena() {
+        assertEquals("cento", NumeroUtils.getGrafiaCentena(1, 2, 0));
+        assertEquals("cem", NumeroUtils.getGrafiaCentena(1, 0, 0));
+        assertEquals("novecentos", NumeroUtils.getGrafiaCentena(9, 3, 1));
+    }
+
+    @Test
+    public void testGetDezena() {
+        assertEquals("dez", NumeroUtils.getGrafiaDezena(1, 0));
+        assertEquals("dezenove", NumeroUtils.getGrafiaDezena(1, 9));
+        assertEquals("trinta", NumeroUtils.getGrafiaDezena(3, 9));
+        assertEquals("noventa", NumeroUtils.getGrafiaDezena(9, 0));
+    }
+
+    @Test
+    public void testGetUnidade2() {
+        assertEquals("zero", NumeroUtils.getGrafiaUnidade(0, 0, 0, 0));
+        assertEquals("nove", NumeroUtils.getGrafiaUnidade(0, 0, 0, 9));
+        assertEquals("dois", NumeroUtils.getGrafiaUnidade(9, 9, 9, 2));
+    }
+
+    @Test
+    public void testGetGrafia() {
+        String resultado = "dois mil";
+        assertEquals(resultado, NumeroUtils.getGrafiaNumeroCardinal(2000));
+
+        resultado = "treze";
+        assertEquals(resultado, NumeroUtils.getGrafiaNumeroCardinal(13));
+
+        resultado = "mil e trezentos";
+        assertEquals(resultado, NumeroUtils.getGrafiaNumeroCardinal(1300));
+
+        resultado = "quinhentos, trinta e cinco";
+        assertEquals(resultado, NumeroUtils.getGrafiaNumeroCardinal(535));
     }
 
 }
